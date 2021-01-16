e2629c9f757635fef6e54f3a90db3a6ed7c76e45
gjh's dad <1185259511@qq.com>
2021-01-16 11:28:29 +0800

    主页项目完成

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,3 +1,14 @@
 $.ajaxPrefilter(function (option) {
     option.url = 'http://api-breakingnews-web.itheima.net' + option.url;
+    if (option.url.indexOf('/my/')) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    option.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+        }
+    }
 })
